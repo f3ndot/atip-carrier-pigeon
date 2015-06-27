111c5c9ea0f2986e2d1081289abef1c185f04c6a
Justin Bull <me@justinbull.ca>
2015-06-27 16:03:12 -0400

    Add User devise model with all the fixin's

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable, maximum_attempts: 5
+
+
+end
